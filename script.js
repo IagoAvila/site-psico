4482b225e8d4a64237da2745d64b4f070b30d1b7
IagoAvila <iagoavilabat@gmail.com>
2025-06-22 00:21:20 -0300

    alt css mob 3

---
+++ script.js
@@ -0,0 +1,16 @@
+const btnTop = document.getElementById("btnTop");
+
+window.onscroll = function() {
+  if (document.documentElement.scrollTop > 100) {
+    btnTop.style.display = "block";
+  } else {
+    btnTop.style.display = "none";
+  }
+};
+
+btnTop.addEventListener("click", function() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
